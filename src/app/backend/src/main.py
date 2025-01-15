4b3105332e8570cf99e2a62d8e04cfbc33373d6e
Alexiya Moore <143016309+Alexiya-M@users.noreply.github.com>
2025-01-15 09:59:22 -0700

    Update main.py

--- src/app/backend/src/main.py
+++ src/app/backend/src/main.py
@@ -1,8 +1,8 @@
 count = 0
 
-while (count > 29):
-  print("Hello World!")
-  count = 0
-  
-else:
+while True:
+  while (count > 29):
+    print("Hello World!")
+    count = 0
+  else:
   count = count + 1
